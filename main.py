a4d2789660dbe2ed079c61e2a50209cffb9fe514
Hans-Peter Bock <hpbock@avaapgh.de>
2019-04-08 21:47:07 +0200

    formatted code using black

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from random import randint
 
 from pirc522 import RFID
 
+
 def tryFindVideosIn(searchPath):
     files = []
     basepath = Path(searchPath)
@@ -16,6 +17,7 @@ def tryFindVideosIn(searchPath):
         if item.is_file():
             files.append(item)
     return files
+
 
 def main():
     rdr = RFID()
@@ -27,25 +29,27 @@ def main():
             (error, uid) = rdr.anticoll()
             if not error:
                 print("UID: " + str(uid))
-                uidString = "{:02x}-{:02x}-{:02x}-{:02x}".format(uid[0], uid[1], uid[2], uid[3])
+                uidString = "{:02x}-{:02x}-{:02x}-{:02x}".format(
+                    uid[0], uid[1], uid[2], uid[3]
+                )
                 print("UID as string: " + uidString)
 
                 videos = tryFindVideosIn(uidString)
 
-                VIDEO_PATH = videos[randint(0, len(videos)-1)]
-    
+                VIDEO_PATH = videos[randint(0, len(videos) - 1)]
+
                 if VIDEO_PATH.is_file():
-                    player = OMXPlayer(VIDEO_PATH, args='-o both')
+                    player = OMXPlayer(VIDEO_PATH, args="-o both")
                     sleep(player.duration())
-    
+
                     if player.can_quit():
                         player.quit()
                 else:
-                    print (str(VIDEO_PATH) + " not found")
-
+                    print(str(VIDEO_PATH) + " not found")
 
     # Calls GPIO cleanup
     rdr.cleanup()
+
 
 if "__main__" == __name__:
     main()
